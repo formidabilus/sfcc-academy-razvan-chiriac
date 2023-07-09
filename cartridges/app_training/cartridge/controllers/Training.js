9efc9c137af3297fcd14ff73ddf873c2d9281638
formidabilus <chiriac_razvan@ymail.com>
2023-07-10 02:49:45 +0300

    solved Creating a Basket JavaScript Controller and Using a Loop in ISML from Module 7

--- cartridges/app_training/cartridge/controllers/Training.js
+++ cartridges/app_training/cartridge/controllers/Training.js
@@ -1,13 +1,30 @@
+// "use strict";
+
+// var server = require("server");
+// var controller = require("app_storefront_base/cartridge/controllers/Cart");
+
+// server.extend(controller);
+
+// server.get("HelloWorld", function(req, res, next) {
+//     var myvariable = "Just a string";
+//     res.render("training/myfirsttemplate", { myvariable: myvariable });
+//     return next();
+// });
+
+// module.exports = server.exports();
+
 "use strict";
 
 var server = require("server");
-var controller = require("app_storefront_base/cartridge/controllers/Cart");
 
-server.extend(controller);
+server.get("Basket", function(req, res, next) {
+    var BasketMgr = require("dw/order/BasketMgr");
 
-server.get("HelloWorld", function(req, res, next) {
-    var myvariable = "Just a string";
-    res.render("training/myfirsttemplate", { myvariable: myvariable });
+    var currentBasket = BasketMgr.getCurrentBasket();
+
+    // Use ISML to display Basket object
+    // The rendered ISML should be showBasket.isml (Use the quickcard section "Giving control to ISML" for help)
+    res.render("training/showBasket", { currentBasket: currentBasket });
     return next();
 });
 
